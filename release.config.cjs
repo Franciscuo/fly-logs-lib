2b3c3c51ef4e726b844afb71b3e80a0799f06062
Francisco Lopez <franlopz10@gmail.com>
2024-10-08 22:50:22 -0500

    fix(greet): fix lib import

--- release.config.cjs
+++ release.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    ['@semantic-release/npm', { pkgRoot: 'dist/' }],
+    ['@semantic-release/npm', { pkgRoot: 'dist' }],
     '@semantic-release/github',
   ],
 };
